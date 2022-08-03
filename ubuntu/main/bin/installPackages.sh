0412d8bb82797235eadf68b39a4599af71a6a3a6
E.Clasen <e.clasen@sportspar.de>
2022-08-03 17:30:36 +0200

    Testing Vagrantfile modularity / reusability

--- ubuntu/main/bin/installPackages.sh
+++ ubuntu/main/bin/installPackages.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# UPDATING REPOSITORY INFO
 sudo apt-get update
-# INSTALLING PACKAGES
 sudo apt-get install -y build-essential libssl-dev libffi-dev apt-transport-https curl python3 python3-pip ansible
+
 if [ ! -e "/usr/bin/ansible" ]; then
   ln -s /usr/local/bin/ansible /usr/bin/ansible;
 fi
+
 exit 0;
-
-
-
