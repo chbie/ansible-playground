b29fd7995be5e0eb62d40f9b812dc07846eb3313
E.Clasen <e.clasen@sportspar.de>
2022-08-18 15:55:52 +0200

    sandbox tested, added as personal / advanced user environment

--- sandbox/showFacts.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /vagrant/ansible || return
-if [[ "$(pwd)" =~ "/vagrant/ansible" ]]; then
-  touch nodes.info
-  echo "$(ANSIBLE_HOST_KEY_CHECKING=False ansible nodes -m setup)" > nodes.info
-  mv -f nodes.info /vagrant/nodes.info
-fi
